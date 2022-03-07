276866b1655a712019a6200c045fa81ba9e8baa0
dimcat <96332975+dimcat@users.noreply.github.com>
2022-03-07 16:26:35 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from mojang import MojangAPI
+
+
+
+re = []
+
+
+
+var = 0
+usernames = ["AcaciaButton", "AcaciaDoor", "AcaciaFence", "AcaciaFenceGate", "AcaciaLeaves", "AcaciaLog", "AcaciaPlanks", "AcaciaPressurePlate", "AcaciaSapling", "AcaciaSign", "AcaciaSlab", "AcaciaStairs", "AcaciaTrapdoor", "AcaciaWood", "ActivatorRail", "Allium", "AmethystCluster", "AncientDebris", "Andesite", "AndesiteSlab", "AndesiteStairs", "AndesiteWall", "Anvil", "Azalea", "AzealeaLeaves", "AzureBluet", "Bamboo", "BambooShoot", "Barrel", "Barrier", "Basalt", "Beacon", "Bedrock", "BeeNest", "Beehive", "Beetroots", "Bell", "BigDripleaf", "BirchButton", "BirchDoor", "BirchFence", "BirchFenceGate", "BirchLeaves", "BirchLog", "BirchPlanks", "BirchPressurePlate", "BirchSapling", "BirchSign", "BirchSlab", "BirchStairs", "BirchTrapdoor", "BirchWood", "BlackBanner", "BlackBed", "BlackCarpet", "BlackConcrete", "BlackConcretePowder", "BlackGlazedTerracotta", "BlackShulkerBox", "BlackStainedGlass", "BlackStainedGlassPane", "BlackTerracotta", "BlackWool", "Blackstone", "BlackstoneSlab", "BlackstoneStairs", "BlackstoneWall", "BlastFurnace", "BlockOfAmethyst", "BlockOfCoal", "BlockOfCopper", "BlockOfDiamond", "BlockOfEmerald", "BlockOfGold", "BlockOfIron", "BlockOfLapisLazuli", "BlockOfNetherite", "BlockOfQuartz", "BlockOfRawCopper", "BlockOfRawGold", "BlockOfRawIron", "BlockOfRedstone"]
+
+
+while "yes" == "yes":    
+    fsf = MojangAPI.get_uuid(usernames[var])
+    if not fsf and len(usernames[var]) < 16:
+        print(usernames[var])
+    
+    var = var + 1
